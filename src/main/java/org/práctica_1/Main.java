58623eb087d8b42f941c353d9bc4afcc8c1d7cdc
al439012 <al439012@uji.es>
2025-02-11 11:54:24 +0100

    gay2

--- src/main/java/org/práctica_1/Main.java
+++ src/main/java/org/práctica_1/Main.java
@@ -1,25 +1,30 @@
 package org.práctica_1;
 package estadistica;
 
+import java.util.List;
 
 public class Estadistica {
-    public static double
-    calcularMedia(List<Double> datos {
-        if (datos.isEmpty)) {
-        throw new IllegalArgumentException("Conjunto de datos vacíos")
+
+    public static double calcularMedia(List<Double> datos) {
+        if (datos.isEmpty()) {
+            throw new IllegalArgumentException("El conjunto de datos está vacío");
+        }
+        double suma = 0;
+        for (double num : datos) {
+            suma += num;
+        }
+        return suma / datos.size();
     }
 
+    public class Main {
+        public static void main(String[] args) {
 
-}
+            System.out.printf("Hello and welcome!");
 
-public class Main {
-    public static void main(String[] args) {
+            for (int i = 1; i <= 5; i++) {
 
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-
-            System.out.println("i = " + i);
+                System.out.println("i = " + i);
+            }
         }
     }
-}
+
