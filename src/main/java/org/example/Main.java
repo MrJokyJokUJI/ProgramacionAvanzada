c44f06e1e21fde2f0726a637f8a0c8afe5fe91bf
al439012 <al439012@uji.es>
2025-02-11 11:28:55 +0100

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,23 @@
+package org.example;
+package estadistica;
+
+
+public class Estadistica {
+    public static double
+    calcularMedia(List<Double> datos {
+        if (datos.isEmpty)
+    })
+
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.printf("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+
+            System.out.println("i = " + i);
+        }
+    }
+}
